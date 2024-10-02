e4d6672e5fee74c55879ebe17b2d411afa9fbabc
Sanjana Hegde <sanjanahegde06@gmail.com>
2024-10-02 10:52:57 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Data Science Project',
+    version= '0.0.0',
+    author= 'Sanjana Shrikant Hegde',
+    author_email= 'sanjanahegde06@gmail.com',
+    packages= find_packages(),
+    install_requires =[]
+
+
+)
